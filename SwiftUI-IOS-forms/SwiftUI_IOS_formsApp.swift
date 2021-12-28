5f06cca966e8afdcd7d83d4430762e7bb4a4c1e3
James Boyer <jamesboyer92@user.noreply.github.com>
2021-12-27 20:37:05 -0500

    Initial Commit

---
+++ SwiftUI-IOS-forms/SwiftUI_IOS_formsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_IOS_formsApp.swift
+//  SwiftUI-IOS-forms
+//
+//  Created by James Boyer on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_IOS_formsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
